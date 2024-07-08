cdfeed1500b51b9f8ffdecf1f2d44bcbe08f2965
P1pp <phil.carl@t-online.de>
2024-07-08 23:18:48 +0200

    add placeholder social-, quests-, home- and  account- page

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PlayertypeComponent } from './playertype.component';
+import { AccountComponent } from './account.component';
 
-describe('PlayertypeComponent', () => {
-  let component: PlayertypeComponent;
-  let fixture: ComponentFixture<PlayertypeComponent>;
+describe('AccountComponent', () => {
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlayertypeComponent]
+      imports: [AccountComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PlayertypeComponent);
+    fixture = TestBed.createComponent(AccountComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
